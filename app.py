8da2ceb9cd0852f144b9200d99df8c355f63d93f
Christopher Alley <calleyviola@gmail.com>
2023-08-25 17:01:38 -0400

    Change api/newgame route

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def new_game():
     game = BoggleGame()
     games[game_id] = game
 
-    return {"gameId": "need-real-id", "board": "need-real-board"}
+    return jsonify(gameId=f"{game_id}", board=f"{game.board}")
+
+# @app.post("/api/score-word")
